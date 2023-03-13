49bfac9b06db34404f68e3bb27e2be204586339d
Luis A. Perez <luis@coreux.io>
2023-03-12 22:49:30 -0500

    Agregas los otros routes

--- routes/web.php
+++ routes/web.php
@@ -30,4 +30,16 @@ Route::middleware([
     Route::get('/productos/{id}', [HomeController::class, 'productoEditar']);
     Route::post('/productos/{id}', [HomeController::class, 'guardarProducto']);
     Route::post('/productos/{id}/borrar', [HomeController::class, 'borrarProducto']);
+
+
+    Route::get('/ciudades', [HomeController::class, 'productosIndex']);
+    Route::get('/ciudades/{id}', [HomeController::class, 'productoEditar']);
+    Route::post('/ciudades/{id}', [HomeController::class, 'guardarProducto']);
+    Route::post('/ciudades/{id}/borrar', [HomeController::class, 'borrarProducto']);
+
+
+    Route::get('/barrios', [HomeController::class, 'productosIndex']);
+    Route::get('/barrios/{id}', [HomeController::class, 'productoEditar']);
+    Route::post('/barrios/{id}', [HomeController::class, 'guardarProducto']);
+    Route::post('/barrios/{id}/borrar', [HomeController::class, 'borrarProducto']);
 });
